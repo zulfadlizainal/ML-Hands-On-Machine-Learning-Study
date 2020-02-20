196492fbca03afde05d1b95072718b41a8552065
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2020-02-20 10:43:38 +0900

    v2.1_Part1_Ch2
    Update codes

---
+++ Part 1 - Fundamentals of Machine Learning/Ch2_E2E Machine Learning Project/Housing Price Project/housing_ch2.py
@@ -0,0 +1,7 @@
+#Practice Code Note by github.com/zulfadlizainal (Based on Hands On Machine Learning Book written by Aurélien Géron)
+
+#Import Library
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
--- Part 1 - Fundamentals of Machine Learning/Ch2_E2E Machine Learning Project/Housing Price Project/housing_ch2_05_preparedata_05transformpipeline.py
+++ Part 1 - Fundamentals of Machine Learning/Ch2_E2E Machine Learning Project/Housing Price Project/housing_ch2_05_preparedata_05transformpipeline.py
@@ -13,7 +13,8 @@ from sklearn.impute import SimpleImputer
 #from sklearn.preprocessing import LabelEncoder
 #from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelBinarizer
-from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 ###################################Check Data###################################
 
@@ -57,26 +58,17 @@ del train_index, test_index
 train_set_prepared = train_set.drop("median_house_value", axis=1)
 train_set_labels = train_set["median_house_value"].copy()
 
+#########################Create Transformation Pipelines#########################
 
-#################Cleaning Data - Using Scikit Learn Imputer#####################
-
-# Define Imputer strategy
-imputer = SimpleImputer(strategy="median")
+# To automate 1) Scaling 2) Imputer (Replace missing Data) 3) Convert Non Numerical data
+# Why important? Any data we input to predict -> we need to apply samme cleanup method as the train data
+# By creationg this pipeline/flow, it will save more time.
 
 # Since Imputer can only process numerical data, we need to seperate text attributes
-train_set_num = train_set_prepared.drop("ocean_proximity", axis=1)
+train_set_prepared_num = train_set_prepared.drop("ocean_proximity", axis=1)
 
-# Fit imputer instance to training data
-clean = SimpleImputer.fit(imputer, train_set_num)
+# Numerical Pipelines
 
-# Now can use the trained Imputer to Transform training set, by replacing missing valued by learned medians
-clean = SimpleImputer.transform(imputer, train_set_num)
-# Output is just numpy array
-
-# Change back to pandas dataframe
-train_set_prepared = pd.DataFrame(clean, columns=train_set_num.columns)
-
-################################Linear Regression###############################
-
-lin_reg = LinearRegression()
-lin_reg = lin_reg.fit(train_set_prepared, train_set_labels)
+num_pipeline = Pipeline([('imputer', SimpleImputer(strategy="median")), (
+    'attribs_adder', CombinedAttributesAdder()), ('std_scaler', StandardScaler()), ])
+train_set_prepared_tr = num_pipeline.fit_transform(train_set_prepared_num)
