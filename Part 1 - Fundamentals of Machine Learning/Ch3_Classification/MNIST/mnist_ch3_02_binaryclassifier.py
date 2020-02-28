a3775686252cf34a369cbaf909622aae49050c37
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2020-02-28 17:18:28 +0900

    v1.2_Part1_Ch3
    Update codes

--- Part 1 - Fundamentals of Machine Learning/Ch3_Classification/MNIST/mnist_ch3_02_binaryclassifier.py
+++ Part 1 - Fundamentals of Machine Learning/Ch3_Classification/MNIST/mnist_ch3_02_binaryclassifier.py
@@ -2,6 +2,7 @@ from sklearn.datasets import fetch_openml
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.linear_model import SGDClassifier
 
 # Load data
 mnist = fetch_openml('mnist_784')
@@ -33,3 +34,16 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 shuffle_index = np.random.permutation(60000)
 
 X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+
+#Try to predict some_digit = 9 using SGD (Stochastic Gradient Descent)
+y_train = y_train.astype(np.int8)
+y_test = y_test.astype(np.int8)
+
+y_train_9 = (y_train == 9)
+y_test_9 = (y_test == 9)
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_9)
+
+#Now can use the SGD to detect the image of number 9
+print(sgd_clf.predict([some_digit]))
