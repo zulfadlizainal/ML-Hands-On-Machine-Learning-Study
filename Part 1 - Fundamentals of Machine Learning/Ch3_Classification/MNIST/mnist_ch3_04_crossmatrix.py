dc8554763a20552134d2978bf551bd2f0456bec8
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2020-03-06 12:01:10 +0900

    v1.3_Part1_Ch3
    Update codes

---
+++ Part 1 - Fundamentals of Machine Learning/Ch3_Classification/MNIST/mnist_ch3_04_crossmatrix.py
@@ -0,0 +1,70 @@
+from sklearn.datasets import fetch_openml
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.base import BaseEstimator
+
+# Load data
+mnist = fetch_openml('mnist_784')
+
+# Looks Data Array - Understand the data
+X, y = mnist['data'], mnist['target']
+print(X.shape)
+print(y.shape)
+
+# Print Any Image
+some_digit = X[36000]
+some_digit_image = some_digit.reshape(28, 28)
+
+plt.imshow(some_digit_image, cmap=matplotlib.cm.binary,
+           interpolation='nearest')
+plt.axis('off')
+plt.show()
+
+# Check the labeled information for that picture
+print(y[36000])
+
+# Plot train data and test data
+# 60000 Images for Training
+# 10000 Images for Test
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+# Shuffle Training data
+# Permutation based on number of Images
+shuffle_index = np.random.permutation(60000)
+
+X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+
+# Try to predict some_digit = 9 using SGD (Stochastic Gradient Descent)
+y_train = y_train.astype(np.int8)
+y_test = y_test.astype(np.int8)
+
+y_train_9 = (y_train == 9)
+y_test_9 = (y_test == 9)
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_9)
+
+# Now can use the SGD to detect the image of number 9
+print(sgd_clf.predict([some_digit]))
+
+# Evaluate model using Cross Evaluation
+print(cross_val_score(sgd_clf, X_train, y_train, cv=3, scoring='accuracy'))
+
+# Try to measure the accuracy base on simple classifier model
+
+
+class Never9Classifier(BaseEstimator):
+    def fit(self, X, y=None):
+        pass
+
+    def predict(self, X):
+        return np.zeros((len(X), 1), dtype=bool)
+
+
+never_9_clf = Never9Classifier()
+print(cross_val_score(never_9_clf, X_train, y_train_9, cv=3, scoring='accuracy'))
+
+################################NEW: START FROM HERE##########################################
