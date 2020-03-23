4effe7c636c458d44869a13ac86dc09b74cf142d
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2020-03-23 12:11:56 +0900

    v1.9_Part1_Ch3
    Update codes

---
+++ Part 1 - Fundamentals of Machine Learning/Ch3_Classification/MNIST/mnist_ch3_10_roccurveSGDvsRF.py
@@ -0,0 +1,102 @@
+from sklearn.datasets import fetch_openml
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
+
+# Load data
+mnist = fetch_openml('mnist_784')
+
+# Looks Data Array - Understand the data
+X, y = mnist['data'], mnist['target']
+print(X.shape)
+print(y.shape)
+
+# Print Any Image
+some_digit = X[36000]
+some_digit_image = some_digit.reshape(28, 28)
+
+plt.imshow(some_digit_image, cmap=matplotlib.cm.binary,
+           interpolation='nearest')
+plt.axis('off')
+plt.show()
+
+# Check the labeled information for that picture
+print(y[36000])
+
+# Plot train data and test data
+# 60000 Images for Training
+# 10000 Images for Test
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+# Shuffle Training data
+# Permutation based on number of Images
+shuffle_index = np.random.permutation(60000)
+
+X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+
+# Try to predict some_digit = 9 using SGD (Stochastic Gradient Descent)
+y_train = y_train.astype(np.int8)
+y_test = y_test.astype(np.int8)
+
+y_train_9 = (y_train == 9)
+y_test_9 = (y_test == 9)
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_9)
+
+################################NEW: START FROM HERE##########################################
+
+# Calculate scores for decision function
+
+y_scores = cross_val_predict(
+    sgd_clf, X_train, y_train_9, cv=3, method='decision_function')
+
+# Calculate FPR and TPR for various thresholds values
+
+fpr, tpr, thresholds = roc_curve(y_train_9, y_scores)
+
+# Plot FPR vs TPR
+
+
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.axis([0, 1, 0, 1])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+
+
+plot_roc_curve(fpr, tpr)
+plt.show()
+
+
+# Calculate ROC AUC Score
+
+auc_score = roc_auc_score(y_train_9, y_scores)
+print(auc_score)
+
+
+# Create ROC for Random Forest Classifier
+
+forest_clf = RandomForestClassifier(random_state=42)
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_9, cv=3, method='predict_proba')
+
+y_scores_forest = y_probas_forest = y_probas_forest[:, 1]
+fpr_forest, tpr_forest, threshold_forest = roc_curve(y_train_9, y_scores_forest)
+
+plt.plot(fpr, tpr, 'b:', label='SGD')
+plot_roc_curve(fpr_forest, tpr_forest, 'Random Forest')
+plt.legend(loc='bottom right')
+plt.show()
+
+auc_score_rf = roc_auc_score(y_train_9, y_scores_forest)
+print(auc_score_rf)
